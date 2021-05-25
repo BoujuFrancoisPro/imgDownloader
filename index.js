fc462bd92e9de89407a94f7ca4a1ceb261d9ac4f
Francois <bouju.francoispro@gmail.com>
2021-05-25 04:27:26 +0200

    did some refactoring

--- index.js
+++ index.js
@@ -1,102 +1,114 @@
 
 const fs = require('fs');
-const { type } = require('os');
 const path = require('path');
 const url = require('url');
 const axios = require('axios');
 
+
+
 class imgDownloader {
 
-    constructor(saveFolderPath){
+    constructor(saveFolderPath) {
         this.saveFolderPath = path.normalize(saveFolderPath);
-        this.fileExtensions = {
-            jpg : ".jpg",
-            png : ".png"
-        }
     }
 
-    //TODO REFACTOR THE SHIT OUT OF IT
-
-    async download(url, fileName){
-        
-        let isDownloaded = false;
+    async download(url, fileName) {
 
         //check if parameters are ok
-        if(typeof url === "undefined" && typeof url !== "string")
-            throw  new TypeError('url parameter expected a string. Received '+ typeof url);
-        if(typeof fileName === "undefined" && typeof fileName !== "string")
-            throw new TypeError('fileName parameter expected a string. Received '+ typeof fileName);
+        if (typeof url === "undefined" && typeof url !== "string")
+            throw new TypeError('url parameter expected a string. Received ' + typeof url);
+        if (typeof fileName === "undefined" && typeof fileName !== "string")
+            throw new TypeError('fileName parameter expected a string. Received ' + typeof fileName);
         
+        //regex to check if the fileneame string is empty or has an unwanted extension
+        let regex = /^[0-9a-zA-Z]+/;
+
+        //if a file extension is found, only keep the name
+        if(!fileName.match(regex))
+            throw new Error('fileName should have no extension and not be empty');
+        else
+            //this will only take into account the name not the extension
+            fileName = fileName.match(regex)[0];
+
+        this.createFolder(this.saveFolderPath);
+
+        // assemble the pull path with all the components folder fileName and the extension from the url
+        let fullPath = path.resolve(this.saveFolderPath, fileName + this.getUrlFileType(url));
+        //check if the full path is already taken if so the function will add the date to the filename
+        fullPath = this.fileExists(fullPath);
+        
+    
+
+        // try to download the image from the url
+        let response;
+        try {
+
+            response = await axios({
+                method: 'GET',
+                url: url,
+                responseType: 'stream'
+            });
+
+        } catch (e) {
+            throw new Error('Could not retrieve image. Error code ' + e.response.status + ' ' + e.response.statusText);
+        }
+
+        // create a write stream pointing on the file
+        let buffer = fs.createWriteStream(fullPath);
+
+        //pipe the response data into it
+        response.data.pipe(buffer)
+        
+        // return a promise that is either resolved when the save is completed 
+        // or rejected when their is an error.
+        return new Promise((resolve, reject) => {
+            buffer.on('error', (err) =>{
+                reject(err);
+            });
+            buffer.on('finish', () =>{
+                resolve(fullPath);
+            });
+        });
+
+    }
+
+    createFolder(saveFolderPath) {
+
+        //returns the path if it was created sucessfully or undefined if it already existed 
+        // Error handling is left the the fs module
+        return fs.mkdirSync(saveFolderPath, { recursive: true });
+    }
+
+    //checks if the file at fullPath already exists, if it does change the fileName of the current file
+    //by adding the date
+    fileExists(fullPath) {
+
+        let parsedFullPath = path.parse(fullPath);
+
+        if (fs.existsSync(fullPath)) {
+
+            let date = new Date(Date.now());
+            var today = date.getDay() + '-' + date.getMonth() + '-' + date.getFullYear() + '-' + date.getSeconds();
+
+            parsedFullPath.name = parsedFullPath.name + '-' + today; 
+            parsedFullPath.base = parsedFullPath.name + parsedFullPath.ext;
+
+            fullPath = path.format(parsedFullPath);
+        }
+
+        return fullPath;
+    }
+
+    //parse the url parameter and extract the filetype
+    getUrlFileType(url){
 
         //get the path porion of the url
         let urlInstance = new URL(url);
         let urlPath = urlInstance.pathname;
 
         //get the fileType from the urlPath
-        let fileType = path.extname(urlPath);
-
-        if(this.checkFileType(fileType)){
-
-            this.createFolder(this.saveFolderPath);
-
-            //assemble the save folder + the file name
-            let fullPath = path.join(this.saveFolderPath, fileName);
-            let parsedFullPath = path.parse(fullPath);
-
-            //if image fileName is already taken add + Date
-            if(fs.existsSync(fullPath)){
-                
-                parsedFullPath.name = parsedFullPath.name + '-' + new Date(Date.now()).toISOString() //TODO fix this date shit
-                parsedFullPath.base = parsedFullPath.name + parsedFullPath.ext;
-
-                fullPath = path.format(parsedFullPath);
-            }
-
-            //download the image from the url
-            //@TODO add error handling
-
-            let buffer = fs.createWriteStream(fullPath);
-
-            let response = await axios({
-                method : 'GET',
-                url : url,
-                responseType : 'stream'
-            });
-
-            //save it at the right location
-            response.data.pipe(buffer)
-            buffer.on('finish', () =>{
-                isDownloaded = true;
-            });
-            
-        }else{
-            throw new Error('This file type is not supported' + fileType);
-        }
-        
-        return isDownloaded
+       return path.extname(urlPath);
     }
-
-    checkFileType(fileType){
-
-        let isSupported = true;
-
-        for(let [key, value] of Object.entries(this.fileExtensions)){
-            if(fileType === value)
-                isSupported = true;
-                break;
-        }
-
-        return isSupported;
-    }
-    
-    createFolder(saveFolderPath){
-        
-        //returns the path if it was created sucessfully or undefined if it already existed 
-        // Error handling is left the the fs module
-        return fs.mkdirSync(saveFolderPath, {recursive : true});
-    }
-
-
 }
 
 module.exports = imgDownloader;
